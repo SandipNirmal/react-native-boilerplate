a7a9351331b8fd68acf02f2349a3b3a60ac04684
Sandip Nirmal <sandipnirmal@icloud.com>
2018-05-07 14:55:46 +0530

    Changes for login screen UI

--- src/components/login.js
+++ src/components/login.js
@@ -1,5 +1,12 @@
 import React, { Component } from 'react';
-import { View, Text, TextInput, Button, StyleSheet } from 'react-native';
+import {
+  View,
+  Text,
+  TextInput,
+  Button,
+  StyleSheet,
+  TouchableOpacity
+} from 'react-native';
 
 class Login extends Component {
   constructor(props) {
@@ -11,27 +18,37 @@ class Login extends Component {
     const { username, password } = this.state;
 
     return (
-      <View className={styles.container}>
-        <View>
+      <View style={styles.container}>
+        <View style={styles.loginForm}>
           <Text> Username / Email : </Text>
           <TextInput
-            style={{ height: 40, borderColor: 'gray', borderWidth: 1 }}
+            style={styles.textInput}
             onChangeText={text => this.setState({ username: text })}
             placeholder="Username/Email"
+            keyboardType="email-address"            
             value={username}
           />
         </View>
-        <View>
-          <Text> Username / Email : </Text>
+        <View style={styles.loginForm}>
+          <Text> Password : </Text>
           <TextInput
-            style={{ height: 40, borderColor: 'gray', borderWidth: 1 }}
+            style={styles.textInput}
             onChangeText={text => this.setState({ password: text })}
             placeholder="Password"
+            secureTextEntry={true}
             value={password}
           />
         </View>
         <View>
-          <Button title="Sign In">Sign In</Button>
+          <TouchableOpacity style={styles.button}>
+            <Button
+              title="Sign In"
+              style={{ color: '#ffffff' }}
+              onPress={() => {
+                console.log('on press');
+              }}
+            />
+          </TouchableOpacity>
         </View>
       </View>
     );
@@ -41,19 +58,54 @@ class Login extends Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
     alignItems: 'center',
-    backgroundColor: '#F5FCFF'
+    justifyContent: 'center',
+    backgroundColor: '#ecf0f1'
   },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10
+  loginForm: {
+    display: 'flex',
+    width: '100%',
+    justifyContent: 'center',
+    alignItems: 'center'
   },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5
+  textInput: {
+    borderColor: '#e1e1e1',
+    borderWidth: 1,
+    textAlign: 'left',
+    height: 40,
+    minWidth: 300,
+    marginVertical: 10,
+    paddingVertical: 4,
+    paddingHorizontal: 6
+  },
+  button: {
+    borderRadius: 25,
+    paddingTop: 5,
+    paddingBottom: 5,
+    paddingLeft: 50,
+    paddingRight: 50,
+    // backgroundColor: '#FFFFFF',
+    backgroundColor: '#2EE59D',
+    shadowColor: 'rgba(0, 0, 0, 0.1)',
+    shadowOpacity: 0.8,
+    elevation: 6,
+    shadowRadius: 15,
+    shadowOffset: { width: 1, height: 13 }
+  },
+  buttonHover: {
+    marginTop: 10,
+    borderRadius: 25,
+    paddingTop: 5,
+    paddingBottom: 5,
+    paddingLeft: 50,
+    paddingRight: 50,
+    shadowColor: 'rgba(46, 229, 157, 0.4)',
+    shadowOpacity: 1.5,
+    elevation: 8,
+    shadowRadius: 20,
+    shadowOffset: { width: 1, height: 13 },
+    backgroundColor: '#2EE59D',
+    color: '#FFFFFF'
   }
 });
 
